8dcfcc2326b0666d7e95c3eddda3ace5b35c024d
EL274 <tresormakosso510@gmail.com>
2024-10-24 13:14:00 +0200

    t

--- part2/hbnb/app/api/__init__.py
+++ part2/hbnb/app/api/__init__.py
@@ -1,1 +1,10 @@
+"""
+API v1 package initialization
+"""
 
+from app.api.v1.users import api as user_api
+from app.api.v1.places import api as place_api
+from app.api.v1.reviews import api as review_api
+from app.api.v1.amenities import api as amenity_api
+
+__all__ = ['user_api', 'place_api', 'review_api', 'amenity_api']
