522a69b2daa302a46e49de5ccddf385e34a4ff00
EL274 <tresormakosso510@gmail.com>
2024-10-24 15:52:40 +0200

    try it

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -3,12 +3,20 @@
 from .basemodel import Basemodel
 from .user import User
 
-class Place(BaseModel):
+class Place(Basemodel):
     def _init_(self, title, description, price, latitude, longitude, owner)
-        super ()_init_()
-        self.title=title 
-        self.description= description
-        self.price= price 
-        self.latitude= latitude 
-        self.longitude= longitude 
-        self.owner= owner
+        super()._init_()
+        self.title =title 
+        self.description = description
+        self.price = price 
+        self.latitude = latitude 
+        self.longitude = longitude 
+        self.owner = owner
+        self.review = []
+        self.ameneties = []
+
+        def add_review(self, review):
+        self.reviews.append(review)
+
+    def add_amenity(self, amenity):
+        self.amenities.append(amenity)
