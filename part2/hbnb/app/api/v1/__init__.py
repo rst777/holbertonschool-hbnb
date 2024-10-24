48c5f347868bf9e344304dc381e2ce801e6eba81
EL274 <tresormakosso510@gmail.com>
2024-10-24 12:15:49 +0200

    just do

--- part2/hbnb/app/api/v1/__init__.py
+++ part2/hbnb/app/api/v1/__init__.py
@@ -1,19 +1,10 @@
-from flask import Blueprint
-from flask_restx import Api
-from app.api.v1.amenities import api as amenities_api
-from app.api.v1.users import api as users_api
+"""
+API v1 package initialization
+"""
 
-# Créez et configurez le blueprint
-blueprint = Blueprint('api_v1', __name__)
+from app.api.v1.users import api as user_api
+from app.api.v1.places import api as place_api
+from app.api.v1.reviews import api as review_api
+from app.api.v1.amenities import api as amenity_api
 
-# Créez l'instance de l'API avec la documentation
-api = Api(
-    blueprint,
-    title='API HBnB',
-    version='1.0',
-    description='Une API pour gérer les utilisateurs et les données associées.'
-)
-
-# Enregistrement des espaces de noms
-api.add_namespace(users_api, path='/users')
-api.add_namespace(amenities_api, path='/amenities')
+__all__ = ['user_api', 'place_api', 'review_api', 'amenity_api']
