59cf17d8d1ecc6a2e1264e447779c0d72353c071
EL274 <tresormakosso510@gmail.com>
2024-10-24 15:44:02 +0200

    what's wrong

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -5,3 +5,10 @@ from .user import User
 
 class Place(BaseModel):
     def _init_(self, title, description, price, latitude, longitude, owner)
+        super ()_init_()
+        self.title=title 
+        self.description= description
+        self.price= price 
+        self.latitude= latitude 
+        self.longitude= longitude 
+        self.owner= owner
