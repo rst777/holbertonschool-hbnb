aade4c52bc6f7cd7a5cdf39784646676b489b485
EL274 <tresormakosso510@gmail.com>
2024-10-24 15:53:38 +0200

    ok

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -1,4 +1,14 @@
 #!/user/bin/python3
 
-from .basemodel import(Basemodel)
+# models/review.py
+from .basemodel import BaseModel
+from .place import Place
+from .user import User
 
+class Review(BaseModel):
+    def __init__(self, text, rating, place, user):
+        super().__init__()
+        self.text = text  # Contenu de l'avis
+        self.rating = rating  # Note donnée (entre 1 et 5)
+        self.place = place  # Lieu associé à l'avis (instance de Place)
+        self.user = user  # Utilisateur qui a écrit l'avis (instance de User)
