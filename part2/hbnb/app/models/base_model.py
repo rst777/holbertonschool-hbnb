ea7dadebd3bc687f5698b1b4183410a5b85d98c4
EL274 <tresormakosso510@gmail.com>
2024-10-24 14:56:20 +0200

    yep

--- part2/hbnb/app/models/base_model.py
+++ part2/hbnb/app/models/base_model.py
@@ -1,1 +1,20 @@
+#!/user/bin/python3
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def update(self, data):
+        for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        self.save()
 
