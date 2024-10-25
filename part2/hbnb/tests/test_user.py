2152b494047adb0a5e2804fa11c0b0869212128b
EL274 <tresormakosso510@gmail.com>
2024-10-25 04:56:10 +0200

    hope

--- part2/hbnb/tests/test_user.py
+++ part2/hbnb/tests/test_user.py
@@ -1,10 +1,13 @@
 #!/user/bin/python3
 
+from app.models.user import User
+
 def test_user_creation():
     user = User(first_name="John", last_name="Doe", email="john.doe@example.com")
     assert user.first_name == "John"
+    assert user.last_name == "Doe"
     assert user.email == "john.doe@example.com"
-    print("Test réussi pour la création d'utilisateur !")
+    assert user.is_admin is False  # Default value
+    print("User creation test passed!")
 
 test_user_creation()
-
