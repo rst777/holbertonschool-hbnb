914a0a931afc50a1c85060bb0441bda1a2d8d31a
EL274 <tresormakosso510@gmail.com>
2024-10-26 00:50:21 +0200

    try and let me know

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+from app.models.user import User
 from app.persistence.repository import InMemoryRepository
 
 class HBnBFacade:
@@ -15,3 +15,24 @@ class HBnBFacade:
     def get_place(self, place_id):
         pass
 
+    def create_user(self, user_data):
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    def get_all_users(self):
+        return self.user_repo.list_all()
+
+    def update_user(self, user_id, user_data):
+        user = self.get_user(user_id)
+        if user:
+            user.update(user_data)  # Hypothèse: méthode d'update définie dans le modèle User
+            self.user_repo.update(user_id, user)
+            return user
+        return None
