b9f414371653040a9ab13b5831673cb9e34c04bd
EL274 <tresormakosso510@gmail.com>
2024-10-25 16:33:14 +0200

    try it

---
+++ part2/hbnb/app/api/v1/places.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class Place:
+    def __init__(self, id, name, location, description):
+        self.id = id
+        self.name = name
+        self.location = location
+        self.description = description
