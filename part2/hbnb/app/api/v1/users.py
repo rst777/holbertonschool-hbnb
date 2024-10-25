bc2a726b7c4f4d572d011f979df7dff0a7b57da1
EL274 <tresormakosso510@gmail.com>
2024-10-25 17:47:00 +0200

    ok

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -1,8 +1,52 @@
 #!/usr/bin/python3
 
+from flask_restx import Namespace, Resource, fields
+from app.services import facade  # Assure-toi que le facade est bien configuré
 
-class User:
-    def __init__(self, id, name, email):
-        self.id = id
-        self.name = name
-        self.email = email
+api = Namespace('users', description='User operations')
+
+# Modèle pour valider les données de l'utilisateur
+user_model = api.model('User', {
+    'first_name': fields.String(required=True, description='Prénom de l\'utilisateur'),
+    'last_name': fields.String(required=True, description='Nom de l\'utilisateur'),
+    'email': fields.String(required=True, description='Email de l\'utilisateur')
+})
+
+@api.route('/')
+class UserList(Resource):
+    @api.expect(user_model, validate=True)
+    @api.response(201, 'Utilisateur créé avec succès')
+    @api.response(400, 'Email déjà enregistré')
+    def post(self):
+        """Enregistre un nouvel utilisateur"""
+        user_data = api.payload
+
+        # Vérification d'unicité de l'email
+        existing_user = facade.get_user_by_email(user_data['email'])
+        if existing_user:
+            return {'error': 'Email déjà enregistré'}, 400
+
+        new_user = facade.create_user(user_data)
+        return {
+            'id': new_user.id,
+            'first_name': new_user.first_name,
+            'last_name': new_user.last_name,
+            'email': new_user.email
+        }, 201
+
+@api.route('/<user_id>')
+class UserResource(Resource):
+    @api.response(200, 'Détails de l\'utilisateur récupérés avec succès')
+    @api.response(404, 'Utilisateur non trouvé')
+    def get(self, user_id):
+        """Récupère les détails d'un utilisateur par ID"""
+        user = facade.get_user(user_id)
+        if not user:
+            return {'error': 'Utilisateur non trouvé'}, 404
+        return {
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email
+        }, 200
+
