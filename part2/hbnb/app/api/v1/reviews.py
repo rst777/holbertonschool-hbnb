ed54bb62a69ca972cbb13a4e47afee861f325225
EL274 <tresormakosso510@gmail.com>
2024-10-25 16:00:41 +0200

    yep

---
+++ part2/hbnb/app/api/v1/reviews.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+class Review:
+    def __init__(self, id, user_id, place_id, text):
+        self.id = id
+        self.user_id = user_id
+        self.place_id = place_id
+        self.text = text
+
