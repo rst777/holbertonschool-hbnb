36523db3223a34d0b05cdd913f0a7b10e13ce40d
EL274 <tresormakosso510@gmail.com>
2024-10-25 15:26:48 +0200

    ok

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 
-from app.services import facade
+from app.persistence.repository import InMemoryRepository
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
 
     def create_user(self, user_data):
-        user = User(**user_data)
-        self.user_repo.add(user)
-        return user
+        pass
 
-    def get_user(self, user_id):
-        return self.user_repo.get(user_id)
+    def get_place(self, place_id):
+        pass
 
-    def get_user_by_email(self, email):
-        return self.user_repo.get_by_attribute('email', email)
