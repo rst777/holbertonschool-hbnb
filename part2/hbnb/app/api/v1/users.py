ac89a886e69287b37e165bb7cc430b0701fdf604
EL274 <tresormakosso510@gmail.com>
2024-10-26 15:24:49 +0200

    ok

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -30,7 +30,7 @@ class UserList(Resource):
         new_user = facade.create_user(user_data)
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
     
-    @api.route('/<user_id>')
+@api.route('/<user_id>')
 class UserResource(Resource):
     @api.response(200, 'User details retrieved successfully')
     @api.response(404, 'User not found')
