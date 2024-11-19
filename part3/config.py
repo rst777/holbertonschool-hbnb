5f30359d5804636d751996a867d9e21cfbd83bca
EL274 <tresormakosso510@gmail.com>
2024-11-19 08:05:45 +0100

    ok

--- part3/config.py
+++ part3/config.py
@@ -4,6 +4,9 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
     SECRET_KEY = 'dev'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key_here')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key_here')
+    
 class DevelopmentConfig(Config):
     DEBUG = True
 
