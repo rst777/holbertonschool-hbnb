1aa2be9d7135c1863b384146b1fed03bcbee298a
EL274 <tresormakosso510@gmail.com>
2024-11-22 12:07:19 +0100

    ok

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -22,7 +22,7 @@ class User(BaseModel):
     
     def set_password(self, password):
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
-    
+        
     def check_password(self , password):
         return bcrypt.check_password_hash(self._password, password)
     
