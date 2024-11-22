f33eae5b6b3a8e81d9068355f2faf9f4e91fcba0
EL274 <tresormakosso510@gmail.com>
2024-11-22 02:40:09 +0100

    ok

--- part3/app/models/db.py
+++ part3/app/models/db.py
@@ -1,8 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
 
 # Configuration de la base de données SQLite
 DATABASE_URL = "sqlite:///hbnb_dev.db"
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@contextmanager
+def get_db_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
